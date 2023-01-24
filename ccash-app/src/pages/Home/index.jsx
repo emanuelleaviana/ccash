96e8f025b97448dcc373e73e870b03feee94504b
Emanuelle Viana <93949785+emanuelleaviana@users.noreply.github.com>
2023-01-24 16:12:57 -0300

    skeletonSite

---
+++ ccash-app/src/pages/Home/index.jsx
@@ -0,0 +1,30 @@
+import './index.css'
+
+export function Home() {
+
+  return (
+    <>
+      <header>
+        <nav className="navBar">
+          <img></img>
+          <a href="#">Início</a>
+          <a href="%">Sobre</a>
+          <a href="%">Serviços</a>
+          <a href="%">FAQ</a>
+          <button>Abra sua conta</button>
+          <button>Acessar</button>
+        </nav>
+      </header>
+
+      <main>
+        <p>Seja bem vindo ao CCA$H!</p>
+        <h1>O banco que cuida de você =)</h1>
+        <p>Os agentes da CCA$S fornecem todo o apoio necessário para você contribuinte que busca atendimentod de qualidade!</p>
+        <button>Saiba Mais</button>
+        <button>Sou Agente</button>
+      </main>
+      
+    </>
+  )
+}
+
