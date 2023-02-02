cd73d29e38570f0ee1199e095806d0b53e09d319
Emanuelle Viana <93949785+emanuelleaviana@users.noreply.github.com>
2023-02-02 10:36:08 -0300

    createMain

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,29 +4,34 @@ export function Home() {
 
   return (
     <>
-      <header>
+      <header className='conteiner'>
         <nav className='navBar'>
-          <p>ccas$h</p>
-          <div className='link'>
+          <p>cca$h</p>
+          <div className='background'>
             <a href="#">Início</a>
             <a href="#">Sobre</a>
             <a href="#">Serviços</a>
             <a href="#">FAQ</a>
           </div>
-          <div>
+          <div className='background'>
             <button>Abra sua conta</button>
             <button>Acessar</button>
           </div>
-
         </nav>
       </header>
 
-      <main>
-        <p>Seja bem vindo ao CCA$H!</p>
-        <h1>O banco que cuida de você =)</h1>
-        <p>Os agentes da CCA$S fornecem todo o apoio necessário para você contribuinte que busca atendimentod de qualidade!</p>
-        <button>Saiba Mais</button>
-        <button>Sou Agente</button>
+      <main className='main'>
+        <div>
+          <p>Seja bem vindo ao CCA$H!</p>
+          <h1>O banco que cuida de você =)</h1>
+          <p>Os agentes da CCA$H fornecem todo o apoio necessário para você contribuinte que busca atendimento de qualidade!</p>
+            <button>Saiba Mais</button>
+            <button>Sou Agente</button>
+          <img id='lines' src="../src/assets/lines.png" alt="" />
+        </div>
+        <div>
+          <img src="../src/assets/woman-computer.png" alt="" />
+        </div>
       </main>
 
     </>
